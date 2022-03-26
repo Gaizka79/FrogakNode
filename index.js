e8ad08b03bc16150f64ee8e21f50b6289e4ad8bb
Gaizka79 <agaizka@hotmail.com>
2022-03-26 02:34:11 +0100

    POST egina

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')  //importamos librerias
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product = require('./models/product')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +15,7 @@ app.use(bodyParser.json())
 /*app.get('/hola/:name', (req,res) => {
     //res.send({ message: 'Hola Mundo!'})
     res.send({ message: `Hola ${req.params.name}!`})
-})*/
+})*/ 
 
 app.get('/api/product', (req, res) => { //Obtener datos
     res.status(200).send({products: []})
@@ -23,9 +26,22 @@ app.get('/api/product/:productID', (req, res) => {  //Obtener datos de un produc
 })
 
 app.post('/api/product', (req, res) => { //Enviar datos
-    console.log(req.body)
-    res.status(200).send({message: 'El producto se ha recibido'})
+    //console.log(req.body)
+    //res.status(200).send({message: 'El producto se ha recibido'})
     //res.status(404).send({message: 'El producto NO se ha recibido'})
+
+    let product = new Product();
+    product.name = req.body.name;
+    product.picture = req.body.picture;
+    product.price = req.body.price;
+    product.category = req.body.category;
+    product.description = req.body.description;
+
+    product.save((err, productStored) => {
+        if (err) res.status(500).send({message: `Error al salvar la BBDD: ${err}`})
+
+        res.status(200).send({product: productStored})
+    })
 })
 
 app.put('/api/product/:productID', (req, res) => {  //Actualizar datos
@@ -36,7 +52,16 @@ app.delete('/api/product/:productID,', (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    //console.log('API REST corriendo en http://localhost:3000')
-    console.log(`API REST corriendo en http://localhost:${port}`)
+mongoose.connect('mongodb://0.0.0.0:27017/shop', (err, res) => {
+    if (err) {
+        console.log(err);
+        return console.log("Error al conectar a la BBDD.")
+    }
+    console.log('Conexión a la BBDD establecida...');
+    app.listen(3000, () => {
+        //console.log('API REST corriendo en http://localhost:3000')
+        console.log(`API REST corriendo en http://localhost:${port}`)
+    })
 })
+
+
